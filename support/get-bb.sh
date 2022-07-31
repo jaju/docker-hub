97632feaceae582b1b087165abb14bd5c7ef34e9
Ravindra Jaju <jaju@sahaj.ai>
2022-07-31 09:38:54 +0530

    Update babashka

--- support/get-bb.sh
+++ support/get-bb.sh
@@ -1,5 +1,5 @@
 #/bin/bash -x
-VERSION=0.8.157
+VERSION=0.9.160
 URL=https://github.com/babashka/babashka/releases/download/v${VERSION}/babashka-${VERSION}-linux-amd64-static.tar.gz
 LOCALFILE=bb-${VERSION}.tar.gz
 
