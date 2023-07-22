fb96cda875ce0a56f8d491c08fc89cbbd8238a80
Ravindra Jaju <jaju@sahaj.ai>
2023-07-22 06:04:49 +0530

    Updated get-bb and get-kafka to download to the support subdirectory irrespective of where they are run from

--- support/get-bb.sh
+++ support/get-bb.sh
@@ -1,9 +1,10 @@
 #/bin/bash -x
-VERSION=1.3.180
+VERSION=1.3.182
 URL=https://github.com/babashka/babashka/releases/download/v${VERSION}/babashka-${VERSION}-linux-amd64-static.tar.gz
 LOCALFILE=bb-${VERSION}.tar.gz
 
+cd "$(dirname "$0")"
 if [ ! -f $LOCALFILE ]; then
-    curl -L $URL -o $LOCALFILE
-    tar zxvf $LOCALFILE
+	curl -L $URL -o $LOCALFILE
+	tar zxvf $LOCALFILE
 fi
--- support/get-kafka.sh
+++ support/get-kafka.sh
@@ -1,11 +1,12 @@
 #/bin/bash -x
 SCALA_VERSION=2.13
-VERSION=3.4.0
+VERSION=3.5.0
 URL=https://dlcdn.apache.org/kafka/${VERSION}/kafka_${SCALA_VERSION}-${VERSION}.tgz
 LOCALFILE=kafka-${VERSION}.tar.gz
 
+cd "$(dirname "$0")"
 if [ ! -f $LOCALFILE ]; then
-    curl -L $URL -o $LOCALFILE
-    tar zxvf $LOCALFILE
-    mv kafka_${SCALA_VERSION}-${VERSION} kafka
+	curl -L $URL -o $LOCALFILE
+	tar zxvf $LOCALFILE
+	mv kafka_${SCALA_VERSION}-${VERSION} kafka
 fi
