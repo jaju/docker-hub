c2815231a6a6b36c6b62f32f87c581917ddd8fd2
Ravindra R. Jaju <jaju@msync.org>
2024-09-21 10:34:25 +0530

    Updates to versions

--- support/get-bb.sh
+++ support/get-bb.sh
@@ -1,9 +1,9 @@
-#/bin/bash -x
-VERSION=1.3.190
+#!/bin/bash -x
+VERSION=1.3.191
 URL=https://github.com/babashka/babashka/releases/download/v${VERSION}/babashka-${VERSION}-linux-amd64-static.tar.gz
 LOCALFILE=bb-${VERSION}.tar.gz
 
-cd "$(dirname "$0")"
+cd "$(dirname "$0")" || exit 1
 if [ ! -f $LOCALFILE ]; then
 	curl -L $URL -o $LOCALFILE
 	tar zxvf $LOCALFILE
--- support/get-kafka.sh
+++ support/get-kafka.sh
@@ -1,10 +1,10 @@
-#/bin/bash -x
+#!/bin/bash -x
 SCALA_VERSION=2.13
-VERSION=3.7.0
+VERSION=3.8.0
 URL=https://dlcdn.apache.org/kafka/${VERSION}/kafka_${SCALA_VERSION}-${VERSION}.tgz
 LOCALFILE=kafka-${VERSION}.tar.gz
 
-cd "$(dirname "$0")"
+cd "$(dirname "$0")" || exit 1
 if [ ! -f $LOCALFILE ]; then
 	curl -L $URL -o $LOCALFILE
 	tar zxvf $LOCALFILE
